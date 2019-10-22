d8809398714c3ccaf34b8db57447dbaa47fd4a98
Vadym Lapach <vl@devurai.com>
2019-10-22 11:15:38 +0300

    temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes} from '@angular/router';
+import {MovieMainComponent} from './movie/movie-main/movie-main.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MovieMainComponent
+  },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieMainComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserAnimationsModule,
+    BrowserModule,
+     RouterModule.forRoot(routes)],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY: '2b04e489',
+  API_URL : 'omdbapi.com  '
 };
 
 /*
