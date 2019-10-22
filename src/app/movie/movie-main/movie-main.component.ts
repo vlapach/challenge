2a94de45e4e6dad69d271c729b979605b0be906c
Vadym Lapach <vl@devurai.com>
2019-10-22 11:16:19 +0300

    temp

---
+++ src/app/movie/movie-main/movie-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-main',
+  templateUrl: './movie-main.component.html',
+  styleUrls: ['./movie-main.component.scss']
+})
+export class MovieMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
